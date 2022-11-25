2d68db2308cdf3cf086d762b40416a8e92709a2f
saschagaudlitz <sascharobert.gaudlitz@alumni.hu-berlin.de>
2022-11-25 12:33:32 +0000

    Update LDM_KDE_v001.py

--- LDM_KDE_v001.py
+++ LDM_KDE_v001.py
@@ -8,21 +8,23 @@ Created on Sun Nov 20 21:28:52 2022
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+'''
 import seaborn as sns
 from scipy.stats import anderson_ksamp
 from scipy.stats import gaussian_kde
-
-''' #makes kernel crash
+'''
+#''' #makes kernel crash
 import torch
 import torch.utils.data as data_utils
 from torch.optim import Adam
 from denoising_diffusion_pytorch import Unet1D, GaussianDiffusion1D
-'''
+from tqdm.auto import tqdm
+#'''
 
 df = pd.read_csv('C:/Users/Sascha/Documents/Data_Challenges/Genhack/Flash/data/df_train.csv',index_col = 0)
 
 X_train = df[['s1', 's2', 's3', 's4', 's5', 's6']].to_numpy()
-
+'''
 #KDE as benchmark Model==========================
 kde = gaussian_kde(X_train.T)
 samples = kde.resample(10**4)
@@ -31,8 +33,8 @@ for i in range(6):
     sns.kdeplot(data = X_train[:,i],fill=True)
     sns.kdeplot(data = samples[i,:])
 #================================================
-
 '''
+#'''
 train = torch.tensor(X_train.astype(np.float32)) 
 train = train.reshape((train.shape[0],1,train.shape[1]))
 train_tensor = data_utils.TensorDataset(train)
@@ -54,7 +56,7 @@ diffusion = GaussianDiffusion1D(
 train_lr = 10**(-3)
 adam_betas = adam_betas = (0.9, 0.99)
 optimizer = Adam(diffusion.parameters(), lr = train_lr, betas = adam_betas)
-num_iter = 2
+num_iter = 10**3
 for batch_ndx in range(num_iter):
     
     sample = next(iter(train_loader))
@@ -63,7 +65,25 @@ for batch_ndx in range(num_iter):
     loss.backward()
     optimizer.step()
     print(batch_ndx)
-        
+
+@torch.no_grad()
+def p_sample_loop(noise):
+    #batch, device = shape[0], self.betas.device
+
+    #img = torch.randn(shape, device=device)
+    img = noise
+
+    x_start = None
+
+    for t in tqdm(reversed(range(0, diffusion.num_timesteps)), desc = 'sampling loop time step', total = diffusion.num_timesteps):
+        self_cond = x_start if diffusion.self_condition else None
+        img, x_start = diffusion.p_sample(img, t, self_cond)
+
+    #img = unnormalize_to_zero_to_one(img)
+    return img   
+
+noise = torch.randn((1,1,6))
+sample = p_sample_loop(noise)     
 #training_seq = torch.randn(8, 1, 6) # features are normalized from 0 to 1
 #loss = diffusion(training_seq)
 #loss = diffusion(train)
@@ -71,14 +91,9 @@ for batch_ndx in range(num_iter):
 
 # after a lot of training
 
-sampled_seq = diffusion.sample(batch_size = 100)
-sampled_seq.shape # (100, 1, 6)
+#sampled_seq = diffusion.sample(batch_size = 100)
+#sampled_seq.shape # (100, 1, 6)
 
 #sns.kdeplot(data=X_train[:, 0], fill=True)
 #sns.kdeplot(data=X_train[:, 0], fill=True)
-'''
-'''
-def f(x):
-    ignore x
-    do y
-'''
+#'''
